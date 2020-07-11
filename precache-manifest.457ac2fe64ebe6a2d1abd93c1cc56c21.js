de997f1d0e756694ed994a6cc874db141f483f7a
Tejaswi2737 <41263776+Tejaswi2737@users.noreply.github.com>
2020-07-12 09:48:59 +1000

    Updates

---
+++ precache-manifest.457ac2fe64ebe6a2d1abd93c1cc56c21.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "362c524ca240fcf9fcbb",
+    "url": "/CovidWebApp/static/css/main.36b7bfd5.chunk.css"
+  },
+  {
+    "revision": "362c524ca240fcf9fcbb",
+    "url": "/CovidWebApp/static/js/main.362c524c.chunk.js"
+  },
+  {
+    "revision": "b07c195eb6291fde6c51",
+    "url": "/CovidWebApp/static/css/1.d9ac1080.chunk.css"
+  },
+  {
+    "revision": "b07c195eb6291fde6c51",
+    "url": "/CovidWebApp/static/js/1.b07c195e.chunk.js"
+  },
+  {
+    "revision": "496d101a144c489b0c76",
+    "url": "/CovidWebApp/static/js/runtime~main.496d101a.js"
+  },
+  {
+    "revision": "74121b1b6a153ca83a8436899c809e16",
+    "url": "/CovidWebApp/static/media/covid.74121b1b.jpg"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/CovidWebApp/static/media/CircularStd-Book.6365c40a.otf"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/CovidWebApp/static/media/CircularStd-Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "4fcdd97fadc3a1d9887f816f2aa67f1d",
+    "url": "/CovidWebApp/static/media/CircularStd-Medium.4fcdd97f.otf"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/CovidWebApp/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/CovidWebApp/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/CovidWebApp/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/CovidWebApp/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/CovidWebApp/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/CovidWebApp/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/CovidWebApp/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/CovidWebApp/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/CovidWebApp/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/CovidWebApp/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/CovidWebApp/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/CovidWebApp/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/CovidWebApp/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/CovidWebApp/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/CovidWebApp/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "802d705d8ba771a49bd76b7bfa64688b",
+    "url": "/CovidWebApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a2877355cfc053b32ae9df7660248f9.js"
+  "/CovidWebApp/precache-manifest.457ac2fe64ebe6a2d1abd93c1cc56c21.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/CovidWebApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
